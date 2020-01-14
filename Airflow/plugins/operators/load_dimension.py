8a7fadf06a12176328918b4618034bae827c4eaa
Shane Latimer <shanealatimer@gmail.com>
2020-01-14 02:55:39 -0800

    Add files via upload

---
+++ Airflow/plugins/operators/load_dimension.py
@@ -0,0 +1,40 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    insert_sql = """
+        TRUNCATE TABLE {};
+        INSERT INTO {}
+        {};
+        COMMIT;
+    """
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 load_sql_stmt="",
+                 truncate=False,
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.load_sql_stmt = load_sql_stmt
+        self.truncate = truncate
+
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if self.truncate:
+           redshift.run(f"TRUNCATE TABLE {self.table}")
+        self.log.info(f"Loading {self.table} in Redshift")
+        formatted_sql = LoadDimensionOperator.insert_sql.format(
+            self.table,
+            self.load_sql_stmt
+        )
+        redshift.run(formatted_sql)
+        self.log.info(f"Success: {self.task_id}")
